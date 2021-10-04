71febaa6435380f3a22eb306bbf72e4000814149
Sydars <xsw5888xsw@gmail.com>
2021-10-04 15:44:26 +0800

    debug imageviewer

--- src/components/ImageViewer/ImageViewer.js
+++ src/components/ImageViewer/ImageViewer.js
@@ -50,7 +50,7 @@ function ImageViewer(props) {
             onClick={() => {
               handleImageClick(index);
             }}
-            src={img}
+            src={img.src}
           />
         ))}
       </Carousel>
