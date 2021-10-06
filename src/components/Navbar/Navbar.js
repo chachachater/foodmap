e65f714bc4b65895b75278e67c5958bcf6ebdcb4
YO-0115 <1024sone@gmail.com>
2021-10-06 21:27:41 +0800

    navbar 修正

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Cookies from "universal-cookie";
 import { logoutAsync, selectUser } from "../../redux/reducers/userReducer";
 import {
   NavbarWrapper,
@@ -13,15 +14,15 @@ import {
 
 function Navbar() {
   const userState = useSelector(selectUser);
-  const user = userState.result
-  let userPath = `/user`
-  if (user) userPath = `/user/${user.data.userId}`
+  const user = userState.result;
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const cookies = new Cookies();
   const dispatch = useDispatch();
   const handleLogout = () => {
-    dispatch(logoutAsync())
-  }
-  return (
+    dispatch(logoutAsync());
+    cookies.remove("getMe");
+  };
+   return (
     <NavbarWrapper>
       <LogoText to="/home">吃貨地圖</LogoText>
       <HamburgerIcon
