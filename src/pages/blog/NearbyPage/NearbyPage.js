b09058eee19ce0d5a264c8e8655abc653a2746f5
Rich Chan <2684awer@gmail.com>
2021-10-29 15:57:24 +0800

    autocomplete bugs

--- src/pages/blog/NearbyPage/NearbyPage.js
+++ src/pages/blog/NearbyPage/NearbyPage.js
@@ -36,7 +36,6 @@ function NearbyPage(props) {
     lat: 25.0428937,
     lng: 121.5054199,
   });
-  const [focused, setFocused] = useState(false);
   const [currentCenter, setCurrentCenter] = useState(myPosition);
   const [photos, setPhotos] = useState(null);
   const [isFold, setIsFold] = useState(true);
@@ -119,9 +118,9 @@ function NearbyPage(props) {
     nearbySearch();
   }, [myPosition]);
 
-  useEffect(() => {
-    if (!focused) setRestaurantList([]);
-  }, [focused]);
+  // useEffect(() => {
+  //   if (!focused) setRestaurantList([]);
+  // }, [focused]);
 
   function searchRestaurantById(placeId, name) {
     if (mapApiLoaded) {
@@ -213,7 +212,6 @@ function NearbyPage(props) {
             inputText={inputText}
             restaurantList={restaurantList}
             handleSearchRestaurant={searchRestaurantById}
-            setFocused={setFocused}
             handleTextSearch={handleTextSearch}
           />
         </SearchBorder>
