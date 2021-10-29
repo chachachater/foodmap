d2f1abce5a996e690577a21794ed228711114cac
outshaker <wind.sixwings@gmail.com>
2021-10-29 14:27:07 +0800

    fix-isValidDate
    - 修正當天 0000-0800 這段時間發食記仍然被判定不是當天的問題
      主因是 new Date(dateString) 的時區為 UTC，這並不是使用者所在的時區。顯示的日期會和使用者認知不相同
    - 將日期格式驗證工作轉嫁給 new Date() 減少程式碼的閱讀成本

--- src/utils.js
+++ src/utils.js
@@ -26,18 +26,10 @@ export function ScrollToTop() {
   return null;
 }
 
+// 驗證食記日期 (符合 yyyy-MM-dd 日期格式, 不超過當天)
 export function isValidDate(dateString) {
   if (!/^\d{4}-\d{1,2}-\d{1,2}$/.test(dateString)) return false;
-  const dateParts = dateString.split("-");
-  const day = parseInt(dateParts[2], 10);
-  const month = parseInt(dateParts[1], 10);
-  const year = parseInt(dateParts[0], 10);
-  if (year < 2000 || month == 0 || month > 12) return false;
-  // 對 2月 28 或 29 天做檢查
-  const monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
-    monthLength[1] = 29;
-  if (!(day > 0 && day <= monthLength[month - 1])) return false;
-  // 年/月/日不能超過今天
-  return new Date(dateString) <= new Date(new Date());
+  const date = new Date(dateString + 'T00:00:00+0800') // 設定為台灣時區的凌晨
+  if (!date) return false
+  return date <= new Date()
 }
